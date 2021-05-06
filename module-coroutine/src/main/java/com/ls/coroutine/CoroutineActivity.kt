c7944ca6b39868c5fde13e4f5470092efa3df402
liushuang <shuang.liu@quvideo.com>
2021-05-06 17:28:44 +0800

    协程异常处理

--- module-coroutine/src/main/java/com/ls/coroutine/CoroutineActivity.kt
+++ module-coroutine/src/main/java/com/ls/coroutine/CoroutineActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.ls.coroutine.page.ExceptionHandlerFragment
 import com.ls.coroutine.page.ScopeFragment
 
 @Route(path = "/coroutine/Index")
@@ -19,6 +20,12 @@ class CoroutineActivity : AppCompatActivity() {
         findViewById<Button>(R.id.btnScope).setOnClickListener {
             val transaction = supportFragmentManager.beginTransaction()
             transaction.add(R.id.fl_container, ScopeFragment.newInstance())
+            transaction.commitAllowingStateLoss()
+        }
+
+        findViewById<Button>(R.id.btnException).setOnClickListener {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.add(R.id.fl_container, ExceptionHandlerFragment.newInstance())
             transaction.commitAllowingStateLoss()
         }
     }
---
+++ module-coroutine/src/main/java/com/ls/coroutine/page/ExceptionHandlerFragment.kt
@@ -0,0 +1,90 @@
+package com.ls.coroutine.page
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.ls.coroutine.R
+import com.ls.coroutine.databinding.FragmentExceptionHandlerBinding
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import java.lang.Exception
+import java.lang.StringBuilder
+
+class ExceptionHandlerFragment : Fragment() {
+
+    private lateinit var binding: FragmentExceptionHandlerBinding
+    private val handler = Handler(Looper.getMainLooper())
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentExceptionHandlerBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        initView()
+    }
+
+    private fun initView() {
+        binding.btnException.setOnClickListener {
+            throwException()
+        }
+    }
+
+    private fun throwException() {
+        val exceptionHandler = CoroutineExceptionHandler {_, _ ->
+            handler.post {
+                appendText("已通过CoroutineExceptionHandler捕获异常\n", binding.tvDesc1)
+            }
+        }
+
+        val exceptionHandler1 = CoroutineExceptionHandler {_, _ ->
+            handler.post {
+                appendText("已通过CoroutineExceptionHandler1----捕获异常\n", binding.tvDesc1)
+            }
+        }
+
+        GlobalScope.launch(exceptionHandler) {
+            /*launch {
+                handler.post {
+                    appendText("子协程抛出了异常\n", binding.tvDesc1)
+                }
+                throw ArithmeticException("Hey!!")
+            }*/
+
+            /**
+             * 这里CoroutineExceptionHandler无法捕获子协程异常
+             */
+            launch(exceptionHandler1) {
+                launch {
+                    handler.post {
+                        appendText("子子协程抛出了异常\n", binding.tvDesc1)
+                    }
+                    //子子协程抛出异常
+                    throw ArithmeticException("Hey!!")
+                }
+            }
+        }
+    }
+
+    private fun appendText(str: String, tv: TextView) {
+        val text = tv.text
+        val builder = StringBuilder()
+        builder.append(text.toString()).append("\n").append(str)
+        tv.text = builder.toString()
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = ExceptionHandlerFragment()
+    }
+}
